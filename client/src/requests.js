109ee5050d6fcde25d6535bd8f6381be84ce03c5
Mengzhe Huang <92949907+mengzhehuang@users.noreply.github.com>
2022-01-02 23:42:02 -0500

    return jobs for a company

--- client/src/requests.js
+++ client/src/requests.js
@@ -58,6 +58,10 @@ export async function loadCompany(id) {
           id
           name
           description
+          jobs {
+            id
+            title
+          }
         }
       }
       `;
